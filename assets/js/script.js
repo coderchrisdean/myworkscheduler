0e71404a3c8c74f72e5a041bf9fee2264001dd9e
coderchrisdean <coderchrisdean@gmail.com>
2022-11-28 21:49:14 -0800

    added code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,309 @@
 // global variables
-var currentDay = dayjs().format('dddd, MMMM D');
-var currentHour = dayjs().format("H");
-var currentTime = dayjs().format("h:mm A");
+var schedule = $("#schedule");
+var currentMilitaryTime = dayjs().format("H");
+var totalHours = 10;
+var currentTime = dayjs().format("h");
 
-var schedule = $("schedule");
-var workHours = 12;
+// setting the current date in the header using dayJS
+function setHeaderDate() {
+  var headerDateEl = $("#current-day");
+  var headerCurrentDate = dayjs().format("dddd, MMM D[th]");
+  headerDateEl.text(headerCurrentDate);
+}
+setHeaderDate();
 
-// 
+//dom work day scheduler
 
-// user opens page, current date/time should show
-// dayjs().format(the day of the week and date)
-// there should be time blocks for work hours in day (setting to 12)
-//separate blocks with divs
-//time block needs to be coded by colors to identify what is past, present, and future
-//create function to apply to share id to the current hour of each block.
+// create div to hold hour blocks for schedule
+function buildHourBlocks() {
+  // the main div which contains the time, task writing area and save button
+  var hourBlock = $(
+    "<div id='hour-block' class='row time-block inactive-block'></div>"
+  );
 
+  // the time shown on each block
+  var hour = $("<div class='col-2 col-md-1 hour text-center py-3'></div>");
+  hourBlock.append(hour);
 
+  // the task writing area
+  var scheduleItem = $(
+    "<textarea class='col-8 col-md-10 description' rows='3' placeholder='Enter Item Here'></textarea>"
+  );
+  hourBlock.append(scheduleItem);
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+  // savebtn and saveicon
+  var saveBtn = $(
+    "<button class='btn saveBtn col-2 col-md-1' aria-label='save'></button>"
+  );
+  var saveIcon = $("<i class='fas fa-save' aria-hidden='true'></i>");
+  saveBtn.append(saveIcon);
+  hourBlock.append(saveBtn);
 
-//inputs time into the header
-$(document).ready(function () {
-
-  $("#currentDay").text(currentDay)
-  $("#currentTime").text(currentTime)
-  console.log (currentHour) //check work
-});
-
-//create divs for blocks of time (DOM)
-
-//creates base div
-
-function buildHourBlock() 
-{
-  // base div w/ time, text input + save btn
-
-  var hourBlock = $("<div id= 'hour-block' class='row time-block inactive-block'></div>")
-  var hour = $("<div class='col-2 col-md-1 hour text-center py-3'></div>")
-  hourBlock.append(hour)
-
-  //text-input
-
-  var scheduleItem = $("<textarea class='col-8 col-md-10 description' rows='3'> </textarea>");
-  hourBlock.append(scheduleItem)
-
-  //save btn/icon
-
-  var saveBtn = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>');
-  var saveIcon = $('<i class="fas fa-save" aria-hidden="true"></i>');
-  hourBlock.append(saveBtn)
-  hourBlock.append(saveIcon)
-
-  // append entire block into div
+  // append the entire hour block to the schedule div
   schedule.append(hourBlock);
 }
 
+// use holding div to build blocks in the work day
+function buildWorkDay() {
+  // for each work hour, create an hour block (each is appended to the schedule as it is created)
+  for (i = 0; i < totalHours; i++) {
+    buildHourBlocks();
+  }
+}
+buildWorkDay();
 
+// Now we have blocks in the schedule representing each hour of the work day. Next, we have to give descriptions and style them.
 
+// First we are going to set the time description for each hour block based on the 12 hour clock.
+function setHourBlockTime() {
+  var thisHour;
+  var amStart = 8;
+  var pmStart = 0;
+  var milDayStart = 8;
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // for each hour of the work day (the # of hour blocks in the schedule)
+  for (i = 0; i <= totalHours; i++) {
+    // find the current hour block as a child of the schedule
+    var hourBlock = $("#schedule").children()[i];
+
+    // iterate through the times (9-12, and 1-5) for each child and set the description;
+    if (amStart <= 11) {
+      // add to morning start until it is noon, then begin the afternoon times.
+      amStart++;
+      thisHour = amStart;
+      $(hourBlock)
+        .find("div")
+        .text(thisHour + " A.M.");
+    } else {
+      pmStart++;
+      thisHour = pmStart;
+      $(hourBlock)
+        .find("div")
+        .text(thisHour + " P.M.");
+    }
+
+    // after we set the visible time description for the block, in the same function we are creating an important data attribute
+    // for each hour block.
+    milDayStart++;
+
+    $(hourBlock).attr("data-work-hour", milDayStart);
+  }
+}
+setHourBlockTime();
+
+// Second, we are going to set styling to change based on whether the work hour block is past, the current work hour, or in the future.
+function setPastPresentFuture() {
+  // the working hours of the day in military time
+  var workingHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+  // The below if statement checks; if our working hours includes the current military time, then we are going to start styling the blocks.
+  // I have it set so that one hour after the work day is over, the blocks reset to an inactive style, to be reactivated one hour
+  // before the next workday.
+  if (workingHours.includes(parseInt(currentMilitaryTime))) {
+    for (i = 0; i <= totalHours; i++) {
+      var hourBlock = $("#schedule").children()[i];
+      // find the value of the work hour data attribute we set for each block. It should start at 9.
+      var hourBlockTimeData = $(hourBlock).attr("data-work-hour");
+
+      // Then, check to see if the work hour data is less than, equal to, or greater than the current military time.
+      // the style will be set by appending and removing the appropriate class.
+      if (parseInt(hourBlockTimeData) < parseInt(currentMilitaryTime)) {
+        $(hourBlock).removeClass("present");
+        $(hourBlock).removeClass("future");
+        $(hourBlock).addClass("past");
+      } else if (
+        parseInt(hourBlockTimeData) === parseInt(currentMilitaryTime)
+      ) {
+        $(hourBlock).removeClass("past");
+        $(hourBlock).removeClass("future");
+        $(hourBlock).addClass("present");
+      } else if (parseInt(hourBlockTimeData) > parseInt(currentMilitaryTime)) {
+        $(hourBlock).removeClass("past");
+        $(hourBlock).removeClass("present");
+        $(hourBlock).addClass("future");
+      } else if (parseInt(currentMilitaryTime) > 18) {
+        $(hourBlock).removeClass("past");
+      }
+    }
+  }
+}
+setPastPresentFuture();
+
+// Now that we have descriptions and styles for each block, next we are going to write code to let us save the tasks.
+
+// First, we create a variable representing the save buttons on the page
+var saveButton = $("button");
+
+// We then create an empty array containing an object representing each hour block, with a blank task.
+// We will be setting and getting this array from local storage to save and display tasks.
+var storedTasks = [
+  {
+    hourBlock: 9,
+    task: "",
+  },
+  {
+    hourBlock: 10,
+    task: "",
+  },
+  {
+    hourBlock: 11,
+    task: "",
+  },
+  {
+    hourBlock: 12,
+    task: "",
+  },
+  {
+    hourBlock: 1,
+    task: "",
+  },
+  {
+    hourBlock: 2,
+    task: "",
+  },
+  {
+    hourBlock: 3,
+    task: "",
+  },
+  {
+    hourBlock: 4,
+    task: "",
+  },
+  {
+    hourBlock: 5,
+    task: "",
+  },
+  {
+    hourBlock: 6,
+    task: "",
+  },
+  {
+    hourBlock: 7,
+    task: "",
+  },
+  {
+    hourBlock: 8,
+    task: "",
+  },
+  {
+    hourBlock: 9,
+    task: "",
+  },
+];
+
+// savebutton click
+$(saveButton).click(function (event) {
+  event.preventDefault();
+
+  var element = event.target;
+
+  // if the user clicks the save icon, then set the element to the save button
+  if (element.matches("i")) {
+    element = $(element).parent();
+  }
+
+  // Find the associated text area of the hour block, based on the save button the user clicked.
+  var textArea = $(element).parent().find("textarea");
+  // Set textAreaInput to the value of the text area
+  var textAreaInput = $(textArea).val();
+  // find the index of the parent hour block in the schedule (very important!)
+  var textAreaIndex = $(element).parent().index();
+
+  // see if there is an array in local storage. If not, then add the saved task to the stored tasks array and save it to local storage.
+  if (localStorage.getItem("storedTasks") === null) {
+    // Since the storedTasks array has the same number of indexes as the schedule (which contains the hour blocks)
+    // value of the index of the hour block to set the appropriate task in stored tasks.
+    storedTasks[textAreaIndex].task = $.trim(textAreaInput);
+    localStorage.setItem("storedTasks", JSON.stringify(storedTasks));
+
+    // If  stored array, retreive from storage, add and save to local storage
+  } else {
+    storedTasks = JSON.parse(localStorage.getItem("storedTasks"));
+    storedTasks[textAreaIndex].task = $.trim(textAreaInput);
+    $(textArea).val(storedTasks[textAreaIndex].task);
+    localStorage.setItem("storedTasks", JSON.stringify(storedTasks));
+  }
+});
+
+//get stored tasks on page reload
+function getStoredTasks() {
+  if (localStorage.getItem("storedTasks") !== null) {
+    for (i = 0; i <= totalHours; i++) {
+      var hourBlockTextarea = $("#schedule").children()[i];
+      storedTasks = JSON.parse(localStorage.getItem("storedTasks"));
+
+      $(hourBlockTextarea).find("textarea").val(storedTasks[i].task);
+    }
+  }
+}
+getStoredTasks();
+
+// A helper function which resets the stored tasks array back to its blank state. If the user has not saved
+// a task before, it will alert them that they have to save a task.
+function clearStoredTasks() {
+  if (localStorage.getItem("storedTasks") !== null) {
+    storedTasks = [
+      {
+        hourBlock: 9,
+        task: "",
+      },
+      {
+        hourBlock: 10,
+        task: "",
+      },
+      {
+        hourBlock: 11,
+        task: "",
+      },
+      {
+        hourBlock: 12,
+        task: "",
+      },
+      {
+        hourBlock: 1,
+        task: "",
+      },
+      {
+        hourBlock: 2,
+        task: "",
+      },
+      {
+        hourBlock: 3,
+        task: "",
+      },
+      {
+        hourBlock: 4,
+        task: "",
+      },
+      {
+        hourBlock: 5,
+        task: "",
+      },
+    ];
+    localStorage.setItem("storedTasks", JSON.stringify(storedTasks));
+  } else {
+    window.alert("You have not saved any tasks!");
+  }
+}
+
+// On click of the clear tasks button, confirm with user then clear the tasks.
+$("#clear-stored-tasks").click(function (event) {
+  event.preventDefault();
+  var confirmClear = window.confirm(
+    "This will clear all of the tasks for today. To clear tasks for one hour, simply erase the task and save. Are you sure you want to continue?"
+  );
+  if (confirmClear) {
+    clearStoredTasks();
+    location.reload();
+  }
+  return;
+});
+
+// At the start of a new work day(1 am), clear the tasks from the previous day.
+function startNewWorkDay() {
+  if (parseInt(currentMilitaryTime) === 1) {
+    clearStoredTasks();
+    location.reload();
+  }
+}
+startNewWorkDay();
